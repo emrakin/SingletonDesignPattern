2d069b9247f5557ee2dc50657518b42d415f0d68
Emrah AKIN <eakin@uzmansistem.com.tr>
2020-11-10 19:14:38 +0300

    Singleton Design Pattern Create

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallel.Invoke(
+                () => Host1GetNextServer(),
+                () => Host2GetNextServer());
+
+            Console.ReadLine();
+        }
+
+        private static void Host1GetNextServer()
+        {
+            Singleton host1List = Singleton.GetTableServers();;
+            host1List.ServerMessage("Hello from host1");
+        }
+
+        private static void Host2GetNextServer()
+        {
+            Singleton host2List = Singleton.GetTableServers();
+            host2List.ServerMessage("Hello from host2");
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPattern
+{
+    public class Singleton
+    {
+        private static Singleton _instance = null;
+        private int nextServer = 0;
+        private static object lockObject = new object();
+
+        private Singleton()
+        {
+            nextServer++;
+            Console.WriteLine($"No, of instance created {nextServer}");
+        }
+
+        public static Singleton GetTableServers()
+        {
+            if(_instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (_instance == null)
+                        _instance = new Singleton();
+                }
+            }
+            return _instance;
+        }
+
+        public void ServerMessage(string message)
+        {
+            Console.WriteLine(message);
+
+        }
+    }
+}
